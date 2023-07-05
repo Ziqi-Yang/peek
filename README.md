2bba2cb95916cab43a2deb0a94c6a0a8e08a85d2
Ziqi Yang <mr.ziqiyang@gmail.com>
2023-07-05 18:36:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Peek
+
+![Static Badge](https://img.shields.io/badge/made_with-emacs-purple)
+
+[project](https://sr.ht/~meow_king/peek/) [mailing lists](https://sr.ht/~meow_king/peek/lists) [tickets](https://sr.ht/~meow_king/peek/trackers)  
+
---
+++ typst-ts-mode.el
@@ -0,0 +1,31 @@
+;;; peek.el --- Peek anything at your fingertip  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Ziqi Yang <mr.meowking@anche.no>
+
+;; Version: 0.1.0
+;; Author: Ziqi Yang <mr.meowking@anche.no>
+;; Keywords: typst languages tree-sitter
+;; URL: https://sr.ht/~meow_king/peek
+;; License: GNU General Public License >= 3
+;; Package-Requires: ((emacs "27")) ;; NOTE haven't been tested
+
+;; This file is NOT part of Emacs.
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'peek)
